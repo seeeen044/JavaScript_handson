38b6eafe730f57ffbb49916e44ae9ebb65150ffb
seeeen044 <chifuyu1228@gmail.com>
2021-05-15 13:22:52 +0900

    fix1 : task 3

--- task3/index.js
+++ task3/index.js
@@ -1,15 +1,17 @@
 const ul = document.getElementById("js-ul");
 
-for(let i = 1; i <= 2; i++){
+const fragment = document.createDocumentFragment();
+const length = 2;
+for(let i = 1; i <= length; i++){
     const li = document.createElement('li');
     const a = document.createElement('a');
     const img = document.createElement('img');
 
-    a.textContent = "a"+i;
-    a.href = "a"+i+".html";
+    a.textContent = `a${i}`;
+    a.href = `a${i}.html`;
     img.src = "/img/bookmark.png";
 
-    ul.appendChild(li).appendChild(a).insertBefore(img, a.firstChild);  
+    fragment.appendChild(li).appendChild(a).insertBefore(img, a.firstChild);  
   }
 
-      
+  ul.appendChild(fragment);
