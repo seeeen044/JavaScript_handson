283d5bf2cbb3f0363594fe2d3249cbfd798bde20
seeeen044 <chifuyu1228@gmail.com>
2021-05-21 16:07:43 +0900

    fix1: task 4

--- task4/index.js
+++ task4/index.js
@@ -6,8 +6,8 @@ const ul = document.getElementById("js-ul");
     {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
   ]
 
-//その1
-Object.values(obj).forEach((value)=>{
+//その1（配列の要素を繰り返し処理する場合に使用するメソッド）
+obj.forEach((value)=>{
     const li = document.createElement('li');
     const a = document.createElement('a');
     const img = document.createElement('img');
@@ -21,32 +21,32 @@ Object.values(obj).forEach((value)=>{
 })
 ul.appendChild(fragment);
 
-//その2
-for (let value in obj) {
-    const li = document.createElement('li');
-    const a = document.createElement('a');
-    const img = document.createElement('img');
+//その2（順不動にループが実行されてしまう、量が多いとエラーになるのであまり使わない）
+// for (let value in obj) {
+//     const li = document.createElement('li');
+//     const a = document.createElement('a');
+//     const img = document.createElement('img');
             
-    a.textContent = obj[value].text;
-    a.href = obj[value].to;
-    img.src = obj[value].img;
-    img.alt = obj[value].alt;
+//     a.textContent = obj[value].text;
+//     a.href = obj[value].to;
+//     img.src = obj[value].img;
+//     img.alt = obj[value].alt;
 
-    fragment.appendChild(li).appendChild(a).insertBefore(img, a.firstChild);
-}
-ul.appendChild(fragment);
+//     fragment.appendChild(li).appendChild(a).insertBefore(img, a.firstChild);
+// }
+// ul.appendChild(fragment);
 
-//その3
-for (let i = 0; i < obj.length; i++) {
-    const li = document.createElement('li');
-    const a = document.createElement('a');
-    const img = document.createElement('img');
+//その3（forEachで書く方が見る人にとって配列のループということがわかりやすい）
+// for (let i = 0; i < obj.length; i++) {
+//     const li = document.createElement('li');
+//     const a = document.createElement('a');
+//     const img = document.createElement('img');
             
-    a.textContent = obj[i].text;
-    a.href = obj[i].to;
-    img.src = obj[i].img;
-    img.alt = obj[i].alt;
+//     a.textContent = obj[i].text;
+//     a.href = obj[i].to;
+//     img.src = obj[i].img;
+//     img.alt = obj[i].alt;
 
-    fragment.appendChild(li).appendChild(a).insertBefore(img, a.firstChild);
-}
-ul.appendChild(fragment);
+//     fragment.appendChild(li).appendChild(a).insertBefore(img, a.firstChild);
+// }
+// ul.appendChild(fragment);
