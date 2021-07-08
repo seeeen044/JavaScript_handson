7c3d9c7e4d83ed2c30b392326f75629e3129c778
seeeen044 <chifuyu1228@gmail.com>
2021-07-08 22:16:50 +0900

    add: task 7

---
+++ task7/index.js
@@ -0,0 +1,34 @@
+const div = document.getElementById("js-loading");
+const gif = document.createElement('img');
+gif.src = "loading-circle.gif";
+div.appendChild(gif);
+
+const getData = new Promise((resolve) => {
+    const obj = [
+        {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"}, 
+        {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
+    ]
+    setTimeout(() => {
+        resolve(obj);
+        gif.remove();
+    }, 2000);
+});
+
+getData.then((value) => {
+    const ul = document.getElementById("js-ul");
+    const fragment = document.createDocumentFragment();
+
+    value.forEach(item => {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        const img = document.createElement('img');
+
+        a.textContent = item.text;
+        a.href = item.to;
+        img.src = item.img;
+        img.alt = item.alt;
+
+        fragment.appendChild(li).appendChild(a).appendChild(img);
+    })
+    ul.appendChild(fragment);
+});
