7209712ceda913ada6cd288cd40785136c033b69
seeeen044 <chifuyu1228@gmail.com>
2021-07-29 13:59:00 +0900

    add: task 8

---
+++ task8/index.js
@@ -0,0 +1,49 @@
+const obj = [
+    {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"}, 
+    {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
+]
+
+const div = document.getElementById("js-parent");
+
+const loading = () => {
+    const loadingPlace = document.createElement("div");
+    const gif = document.createElement('img');
+    loadingPlace.id = "loadingPlace";
+    gif.src = "loading-circle.gif";
+    div.appendChild(loadingPlace).appendChild(gif);
+};
+
+const loaded = () => {
+    document.getElementById("loadingPlace");
+    loadingPlace.remove();
+}
+    
+const getData  = new Promise((resolve, reject) => {
+    loading();
+        setTimeout(() => {
+        reject(obj);
+    },3000);
+});
+
+getData.then((value) => {
+    loaded();
+    const ul = document.createElement('ul');
+    const fragment = document.createDocumentFragment();
+
+    value.forEach(item => {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        const img = document.createElement('img');
+
+        a.textContent = item.text;
+        a.href = item.to;
+        img.src = item.img;
+        img.alt = item.alt;
+
+        fragment.appendChild(li).appendChild(a).appendChild(img);
+    })
+    div.appendChild(ul).appendChild(fragment);
+});
+getData.catch(() => {
+    console.log("error!");
+});
