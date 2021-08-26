4ca9d6cf8a8265a67f1f664f128bb38d7fd45e9d
seeeen044 <chifuyu1228@gmail.com>
2021-08-26 21:59:52 +0900

    add: task 11

---
+++ task11/index.js
@@ -0,0 +1,51 @@
+const div = document.getElementById("js-parent");
+
+const loading = () => {
+    const loadingPlace = document.createElement("div");
+    const gif = document.createElement('img');
+    loadingPlace.id = "loadingPlace";
+    gif.src = "loading-circle.gif";
+    div.appendChild(loadingPlace).appendChild(gif);
+};
+
+const removeLoading = () => {
+    document.getElementById("loadingPlace").remove();
+};
+
+const getData = async () => {
+    loading();
+    try {
+        const request = await fetch("https://jsondata.okiba.me/v1/json/3jhPW210812052923");
+        const data = request.json();
+        return data;
+        
+    } catch (error) {
+        div.textContent = "データを読み込めませんでした";
+        console.error(error);
+    }
+    finally{
+        removeLoading();
+    };
+};
+
+const objShow = async () => {
+    const value = await getData();
+
+    const ul = document.createElement('ul');
+    const fragment = document.createDocumentFragment();
+   
+   value.data.forEach(key => {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        const img = document.createElement('img');
+   
+        a.textContent = key.text;
+        a.href = key.a;
+        img.src = key.img;
+        img.alt = key.alt;
+   
+        fragment.appendChild(li).appendChild(a).appendChild(img);
+    })
+    div.appendChild(ul).appendChild(fragment);  
+};
+objShow();
