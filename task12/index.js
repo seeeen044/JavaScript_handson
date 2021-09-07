d7b5953abc367569b30f2bc66a5147334ac15bd5
seeeen044 <chifuyu1228@gmail.com>
2021-09-07 12:34:13 +0900

    add: task 12

---
+++ task12/index.js
@@ -0,0 +1,56 @@
+const parent = document.getElementById("js-parent");
+    
+const appendButtonForParent = () => {
+    const button = document.createElement('input');
+    button.id = "button";
+    button.type = "button";
+    button.value = "click";
+    parent.appendChild(button);
+};
+appendButtonForParent();
+
+const removeButton = () => {
+    document.getElementById("button").remove();
+};
+
+const addEvent = document.getElementById("button"); 
+addEvent.addEventListener('click', () => {
+    removeButton();
+    init();
+});
+
+const getData = async () => {
+    try {
+        const request = await fetch("https://jsondata.okiba.me/v1/json/3jhPW210812052923");
+        const data = request.json();
+        return data;
+            
+    } catch (error) {
+        parent.textContent = "データを読み込めませんでした";
+        console.error(error);
+    }
+};
+
+const appendListForParent = (value) => {
+    const ul = document.createElement('ul');
+    const fragment = document.createDocumentFragment();
+    
+    value.data.forEach(key => {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        const img = document.createElement('img');
+    
+        a.textContent = key.text;
+        a.href = key.a;
+        img.src = key.img;
+        img.alt = key.alt;
+    
+        fragment.appendChild(li).appendChild(a).appendChild(img);
+    })
+    parent.appendChild(ul).appendChild(fragment);  
+};
+
+const init = async () => {
+    const value = await getData();
+    appendListForParent(value);
+};
