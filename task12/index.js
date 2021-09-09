5cc3f88281eebf9ab2ff92b34a0aa6289a4f9d20
seeeen044 <chifuyu1228@gmail.com>
2021-09-09 22:06:34 +0900

    feat: modify variable name

--- task12/index.js
+++ task12/index.js
@@ -23,8 +23,8 @@ button.addEventListener('click', (e) => {
 
 const getData = async () => {
     try {
-        const request = await fetch("https://jsondata.okiba.me/v1/json/3jhPW210812052923");
-        const data = request.json();
+        const response = await fetch("https://jsondata.okiba.me/v1/json/3jhPW210812052923");
+        const data = response.json();
         return data;
             
     } catch (error) {
@@ -36,17 +36,17 @@ const getData = async () => {
 const appendListForParent = (value) => {
     const ul = document.createElement('ul');
     const fragment = document.createDocumentFragment();
-    
+        
     value.data.forEach(key => {
         const li = document.createElement('li');
         const a = document.createElement('a');
         const img = document.createElement('img');
-    
+        
         a.textContent = key.text;
         a.href = key.a;
         img.src = key.img;
         img.alt = key.alt;
-    
+        
         fragment.appendChild(li).appendChild(a).appendChild(img);
     })
     parent.appendChild(ul).appendChild(fragment);  
