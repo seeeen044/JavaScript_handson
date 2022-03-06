4a30dbe4886d1a19551d7646892ee72784929736
seeeen044 <chifuyu1228@gmail.com>
2022-03-06 11:48:11 +0900

    add:getFetchData and init function

--- task16/index.js
+++ task16/index.js
@@ -1,16 +1,38 @@
-const tabSwitch = () => {
-    const tabMenu = document.getElementsByClassName('js-tabMenu');
-        for(let i = 0; i < tabMenu.length; i++){
-            tabMenu[i].addEventListener('click', (e) => {
-            document.getElementsByClassName('active')[0].classList.remove('active');
-            e.target.classList.add('active');
-            document.getElementsByClassName('show')[0].classList.remove('show');
-            const arrayTabMenu = Array.prototype.slice.call(tabMenu);
-            const index = arrayTabMenu.indexOf(e.target);
-            const tabContents = document.getElementsByClassName('js-tabContents');
-            tabContents[index].classList.add('show'); 
-        });
-    };
+const URL = "https://myjson.dit.upm.es/api/bins/as8f";
+const tabWrapper = document.getElementById("js-tabWrapper");
+const tab = document.getElementById("js-tabMenu");
+
+const createElementWithClassName = (element, name) => {
+    const createdElement = document.createElement(element);
+    createdElement.classList.add(name);
+    return createdElement;
 };
-tabSwitch();
+
+const getFetchData = async () => {
+    try{
+        const respons = await fetch(URL);
+        if(!respons.ok){
+            throw new Error('ただいまサーバー側で通信が壊れています')
+        };
+        const json = await respons.json();
+        return json;
+    }
+    catch(error) {
+        console.error(error);
+    }
+};
+
+const init = async () => {
+    try {
+        const value = await getFetchData();
+        renderTabMenu(value);
+        renderTabContainer(value);
+        tabSwitch();
+    }
+    catch(error) {
+        console.error(error);
+        tab.textContent = "データを読み込めませんでした";
+    }
+};
+init();
 
