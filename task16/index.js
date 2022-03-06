2d6d45ed8d779cab455fecdb6efe017a6f8e9f63
seeeen044 <chifuyu1228@gmail.com>
2022-03-06 11:50:24 +0900

    add:renderTabContainer function

--- task16/index.js
+++ task16/index.js
@@ -48,3 +48,23 @@ const renderTabMenu = (value) => {
     const addClassActive = document.getElementById("js-tabMenuList0");
     addClassActive.classList.add("active");
 };
+
+const renderTabContainer = (value) => {
+    const tabContentsFragment = document.createDocumentFragment();
+    for(let i = 0; i < value.length; i++){
+        const newsWrapper = createElementWithClassName('div', "news-wrapper");
+        const newsTitlelist = createElementWithClassName('ul', "news-title-list");
+        const newsImg = createElementWithClassName('img', "news-img")
+        newsWrapper.id = `js-newsWrapper${i}`;
+        newsImg.src = value[i].img;
+
+        tabContentsFragment.appendChild(newsWrapper).appendChild(newsTitlelist).appendChild(getNewsTitle(value[i]));
+
+        tabContentsFragment.appendChild(newsWrapper).appendChild(newsImg);
+    }
+    const tabContainer = createElementWithClassName('div', "tab-container");
+    tabWrapper.appendChild(tabContainer).appendChild(tabContentsFragment);
+    const addClassShow = document.getElementById("js-newsWrapper0");
+    addClassShow.classList.add("show");
+};
+
