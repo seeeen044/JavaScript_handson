f740b9d0c2aab0d1942c63216a32a19c6e4fe6da
seeeen044 <chifuyu1228@gmail.com>
2022-03-07 21:53:17 +0900

    refactor:uniformity of indentaition and fix CSS in news titles

--- task16/index.js
+++ task16/index.js
@@ -3,106 +3,105 @@ const tabWrapper = document.getElementById("js-tabWrapper");
 const tab = document.getElementById("js-tabMenu");
 
 const createElementWithClassName = (element, name) => {
-    const createdElement = document.createElement(element);
-    createdElement.classList.add(name);
-    return createdElement;
+  const createdElement = document.createElement(element);
+  createdElement.classList.add(name);
+  return createdElement;
 };
 
 const addLoading = () => {
-    const loadingPlace = createElementWithClassName('div', "loader");
-    loadingPlace.id = "loadingPlace";
-    tabWrapper.appendChild(loadingPlace);
-}
+  const loadingPlace = createElementWithClassName("div", "loader");
+  loadingPlace.id = "loadingPlace";
+  tabWrapper.appendChild(loadingPlace);
+};
 
 const removeLoading = () => document.getElementById("loadingPlace").remove();
 
 const getFetchData = async () => {
-    try{
-        const respons = await fetch(URL);
-        if(!respons.ok){
-            throw new Error('ただいまサーバー側で通信が壊れています')
-        };
-        const json = await respons.json();
-        return json;
+  try {
+    const respons = await fetch(URL);
+    if (!respons.ok) {
+      throw new Error("ただいまサーバー側で通信が壊れています");
     }
-    catch(error) {
-        console.error(error);
-    }
+    const json = await respons.json();
+    return json;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 const init = async () => {
-    addLoading();
-    try {
-        const value = await getFetchData();
-        renderTabMenu(value);
-        renderTabContainer(value);
-        // tabSwitch();
-    }
-    catch(error) {
-        console.error(error);
-        tab.textContent = "データを読み込めませんでした";
-    }
-    finally {
-        removeLoading();
-    }
+  addLoading();
+  try {
+    const value = await getFetchData();
+    renderTabMenu(value);
+    renderTabContainer(value);
+    // tabSwitch();
+  } catch (error) {
+    console.error(error);
+    tab.textContent = "データを読み込めませんでした";
+  } finally {
+    removeLoading();
+  }
 };
 init();
 
 const renderTabMenu = (value) => {
-    const tabMenuFragment = document.createDocumentFragment();
-    for(let i = 0; i < value.length; i++){
-        const menuList = createElementWithClassName('li', "menu-list");
-        menuList.id = `js-tabMenuList${i}`;
-        menuList.textContent = value[i].category;
-        tabMenuFragment.appendChild(menuList);
-    }
-    tab.appendChild(tabMenuFragment);
-    const addClassActive = document.getElementById("js-tabMenuList0");
-    addClassActive.classList.add("active");
+  const tabMenuFragment = document.createDocumentFragment();
+  for (let i = 0; i < value.length; i++) {
+    const menuList = createElementWithClassName("li", "menu-list");
+    menuList.id = `js-tabMenuList${i}`;
+    menuList.textContent = value[i].category;
+    tabMenuFragment.appendChild(menuList);
+  }
+  tab.appendChild(tabMenuFragment);
+  const addClassActive = document.getElementById("js-tabMenuList0");
+  addClassActive.classList.add("active");
 };
 
 const renderTabContainer = (value) => {
-    const tabContentsFragment = document.createDocumentFragment();
-    for(let i = 0; i < value.length; i++){
-        const newsWrapper = createElementWithClassName('div', "news-wrapper");
-        const newsTitlelist = createElementWithClassName('ul', "news-title-list");
-        const newsImg = createElementWithClassName('img', "news-img")
-        newsWrapper.id = `js-newsWrapper${i}`;
-        newsImg.src = value[i].img;
+  const tabContentsFragment = document.createDocumentFragment();
+  for (let i = 0; i < value.length; i++) {
+    const newsWrapper = createElementWithClassName("div", "news-wrapper");
+    const newsTitlelist = createElementWithClassName("ul", "news-title-list");
+    const newsImg = createElementWithClassName("img", "news-img");
+    newsWrapper.id = `js-newsWrapper${i}`;
+    newsImg.src = value[i].img;
 
-        tabContentsFragment.appendChild(newsWrapper).appendChild(newsTitlelist).appendChild(getNewsTitle(value[i]));
+    tabContentsFragment
+      .appendChild(newsWrapper)
+      .appendChild(newsTitlelist)
+      .appendChild(getNewsTitle(value[i]));
 
-        tabContentsFragment.appendChild(newsWrapper).appendChild(newsImg);
-    }
-    const tabContainer = createElementWithClassName('div', "tab-container");
-    tabWrapper.appendChild(tabContainer).appendChild(tabContentsFragment);
-    const addClassShow = document.getElementById("js-newsWrapper0");
-    addClassShow.classList.add("show");
+    tabContentsFragment.appendChild(newsWrapper).appendChild(newsImg);
+  }
+  const tabContainer = createElementWithClassName("div", "tab-container");
+  tabWrapper.appendChild(tabContainer).appendChild(tabContentsFragment);
+  const addClassShow = document.getElementById("js-newsWrapper0");
+  addClassShow.classList.add("show");
 };
 
-const getNewsTitle = ({article}) => {
-    const newsTitleFragment = document.createDocumentFragment();
-    for(let i = 0; i < article.length; i++){
-        const newsTitleItem = createElementWithClassName('li', "news-title-item");
-        const newsLink = createElementWithClassName('a', "news-link");
-        newsLink.textContent = article[i].title;
-        newsLink.href = "#";
+const getNewsTitle = ({ article }) => {
+  const newsTitleFragment = document.createDocumentFragment();
+  for (let i = 0; i < article.length; i++) {
+    const newsTitleItem = createElementWithClassName("li", "news-title-item");
+    const newsLink = createElementWithClassName("a", "news-link");
+    newsLink.textContent = article[i].title;
+    newsLink.href = "#";
 
-        newsTitleFragment.appendChild(newsTitleItem).appendChild(newsLink);
-    }
-    return newsTitleFragment;
+    newsTitleFragment.appendChild(newsTitleItem).appendChild(newsLink);
+  }
+  return newsTitleFragment;
 };
 
-    tab.addEventListener('click', (e) => {
-    const active = document.querySelector(".active");
-    const show = document.querySelector(".show");
-    active.classList.remove("active");
-    e.target.classList.add("active");
-    const tabMenu = document.querySelectorAll(".menu-list")
-    const arrayTabMenu = Array.prototype.slice.call(tabMenu);
-    const index = arrayTabMenu.indexOf(e.target);
-    show.classList.remove("show");
-    const tabContent = document.getElementsByClassName("news-wrapper");
-    tabContent[index].classList.add("show");
-    });
-    
+tab.addEventListener("click", (e) => {
+  const active = document.querySelector(".active");
+  const show = document.querySelector(".show");
+  active.classList.remove("active");
+  e.target.classList.add("active");
+  const tabMenu = document.querySelectorAll(".menu-list");
+  const arrayTabMenu = Array.prototype.slice.call(tabMenu);
+  const index = arrayTabMenu.indexOf(e.target);
+  show.classList.remove("show");
+  const tabContent = document.getElementsByClassName("news-wrapper");
+  tabContent[index].classList.add("show");
+});
