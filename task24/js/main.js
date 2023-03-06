22ae208b1021a507ebcac0d073754304b3540df4
seeeen044 <chifuyu1228@gmail.com>
2023-03-06 12:41:16 +0900

    refactor:Remove unused variables and correct typos

--- task24/js/main.js
+++ task24/js/main.js
@@ -1,11 +1,9 @@
 import "../css/style.css";
 
 const modalWrapper = document.getElementById("js-modalContents");
-const modal = document.getElementById("js-modal");
 const modalContents = document.getElementById('js-modalContents');
 const modalPlace = document.getElementById("js-modalPlace");
 const closeModal = document.getElementById("js-modalClose");
-const closeModalText = document.getElementById("js-modalCloseText");
 const checkBox = document.getElementById("js-checkBox");
 const login = document.getElementById("js-login");
 
@@ -20,14 +18,14 @@ const options = {
     root: modalContents,
   }
 
-const checkedvalue = ([entry]) => {
+const checkedValue = ([entry]) => {
     if(entry.isIntersecting){
         checkBox.checked = true;
         checkBox.disabled = false;
     } 
 };
 
-const observer = new IntersectionObserver(checkedvalue, options);
+const observer = new IntersectionObserver(checkedValue, options);
 observer.observe(closeModal);
 
 login.addEventListener("click" , (e) => {
