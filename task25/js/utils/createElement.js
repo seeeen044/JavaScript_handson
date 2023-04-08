c18173a89e03de4759703bd91ace136b9fa3b819
seeeen044 <chifuyu1228@gmail.com>
2023-04-08 23:30:55 +0900

    remove:Delete Utils folder

--- task25/js/utils/createElement.js
+++
@@ -1,6 +0,0 @@
-export const createElementWithClassName = (element, name) => {
-    const createdElement = document.createElement(element);
-    createdElement.classList.add(name);
-    return createdElement;
-  };
-  
